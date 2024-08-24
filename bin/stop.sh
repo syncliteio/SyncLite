3621cab639d5b27843ae5c92a69a28ddeee4f959
mahendra-chavan <mahendra@synclite.io>
2024-08-24 11:24:07 +0530

    Fixed stop script to correctly stop SyncLite process and tomcat process.

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
-pid=$("$JAVA_HOME/bin/jps" -l | grep com.synclite.consolidator.Main | awk '{print $1}')
-if [[ -n $pid ]]; then
-  kill "$pid"
-fi
+PARENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-pid=$("$JAVA_HOME/bin/jps" -l | grep org.apache.catalina.startup.Bootstrap | awk '{print $1}')
-if [[ -n $pid ]]; then
-  kill "$pid"
-fi
+export JAVA_HOME="$PARENT_DIR/jdk-11"
+
+# Function to find and kill process by name using ps -eaf
+kill_process_by_name() {
+  local process_name=$1
+  local pid
+
+  pid=$(ps -eaf | grep "$process_name" | grep -v grep | awk '{print $2}')
+  if [[ -n $pid && $pid =~ ^[0-9]+$ ]]; then
+    kill -9 "$pid"
+    echo "Killed process $pid ($process_name)"
+  else
+    echo "No valid process found for $process_name"
+  fi
+}
+
+# Kill the Java processes
+kill_process_by_name "com.synclite.consolidator.Main"
+kill_process_by_name "com.synclite.dbreader.Main"
+kill_process_by_name "com.synclite.qreader.Main"
+kill_process_by_name "org.apache.catalina.startup.Bootstrap"
+
