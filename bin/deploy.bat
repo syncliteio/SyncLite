a4959f4afb36def1c5abe25a235c490e7a106692
mahendra-chavan <mahendra@synclite.io>
2024-10-01 18:19:17 +0530

    Release 2024.10.01

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,18 +1,18 @@
 @echo off
 
 REM Set the URL of the zip file to download
-set URL=https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.93/bin/apache-tomcat-9.0.93-windows-x64.zip
+set URL=https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.95/bin/apache-tomcat-9.0.95-windows-x64.zip
 set "PARENT_DIR=%~dp0"
 
-curl -o apache-tomcat-9.0.93.zip %URL%
+curl -o apache-tomcat-9.0.95.zip %URL%
 
 REM Extract the zip file using expand
-powershell -Command "Add-Type -A 'System.IO.Compression.FileSystem'; [System.IO.Compression.ZipFile]::ExtractToDirectory('apache-tomcat-9.0.93.zip', '%PARENT_DIR%')"
+powershell -Command "Add-Type -A 'System.IO.Compression.FileSystem'; [System.IO.Compression.ZipFile]::ExtractToDirectory('apache-tomcat-9.0.95.zip', '%PARENT_DIR%')"
 
 REM Copy tomcat-users.xml file into extracted tomcat folder
-echo F | xcopy /Y tomcat-users.xml apache-tomcat-9.0.93\conf\tomcat-users.xml
+echo F | xcopy /Y tomcat-users.xml apache-tomcat-9.0.95\conf\tomcat-users.xml
 
-del apache-tomcat-9.0.93.zip
+del apache-tomcat-9.0.95.zip
 
 REM Set the URL of the zip file to download
 set URL=https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_windows-x64_bin.zip
@@ -32,8 +32,8 @@ powershell -Command "Add-Type -A 'System.IO.Compression.FileSystem'; [System.IO.
 REM Clean up the downloaded zip file
 del openjdk-11+28_windows-x64_bin.zip
 
-echo F | xcopy /Y ..\lib\consolidator\synclite-consolidator-*.war apache-tomcat-9.0.93\webapps\synclite-consolidator.war
-echo F | xcopy /Y ..\sample-apps\jsp-servlet\web\target\*.war apache-tomcat-9.0.93\webapps\synclite-sample-app.war
-echo F | xcopy /Y ..\tools\synclite-dbreader\*.war apache-tomcat-9.0.93\webapps\synclite-dbreader.war
-echo F | xcopy /Y ..\tools\synclite-qreader\*.war apache-tomcat-9.0.93\webapps\synclite-qreader.war
-echo F | xcopy /Y ..\tools\synclite-jobmonitor\*.war apache-tomcat-9.0.93\webapps\synclite-jobmonitor.war
+echo F | xcopy /Y ..\lib\consolidator\synclite-consolidator-*.war apache-tomcat-9.0.95\webapps\synclite-consolidator.war
+echo F | xcopy /Y ..\sample-apps\synclite-logger\jsp-servlet\web\target\*.war apache-tomcat-9.0.95\webapps\synclite-sample-app.war
+echo F | xcopy /Y ..\tools\synclite-dbreader\*.war apache-tomcat-9.0.95\webapps\synclite-dbreader.war
+echo F | xcopy /Y ..\tools\synclite-qreader\*.war apache-tomcat-9.0.95\webapps\synclite-qreader.war
+echo F | xcopy /Y ..\tools\synclite-jobmonitor\*.war apache-tomcat-9.0.95\webapps\synclite-jobmonitor.war
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 # Set the URL of the tar file to download
-URL="https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.93/bin/apache-tomcat-9.0.93.tar.gz"
+URL="https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.95/bin/apache-tomcat-9.0.95.tar.gz"
 # Set the destination directory for the downloaded tar file
 DESTINATION="."
 
 # Download the tar file using curl
-wget -O "$DESTINATION/apache-tomcat-9.0.93.tar.gz" "$URL" --no-check-certificate
+wget -O "$DESTINATION/apache-tomcat-9.0.95.tar.gz" "$URL" --no-check-certificate
 
 # Extract the zip file using unzip
-tar -xzf "$DESTINATION/apache-tomcat-9.0.93.tar.gz" -C "$DESTINATION"
+tar -xzf "$DESTINATION/apache-tomcat-9.0.95.tar.gz" -C "$DESTINATION"
 
 # Copy tomcat-users.xml file into extracted tomcat folder
-cp -f tomcat-users.xml apache-tomcat-9.0.93/conf/tomcat-users.xml
+cp -f tomcat-users.xml apache-tomcat-9.0.95/conf/tomcat-users.xml
 
 # Clean up the downloaded zip file
-rm $DESTINATION/apache-tomcat-9.0.93.zip
+rm $DESTINATION/apache-tomcat-9.0.95.zip
 
 
 # Set the URL of the tar file to download
@@ -33,9 +33,9 @@ tar -xzf "$DESTINATION/openjdk-11+28_linux-x64_bin.tar.gz" -C "$DESTINATION"
 # Clean up the downloaded zip file
 rm $DESTINATION/openjdk-11+28_linux-x64_bin.tar.gz
 
-cp -f ../lib/consolidator/synclite-consolidator-*.war apache-tomcat-9.0.93/webapps/synclite-consolidator.war
-cp -f ../sample-apps/jsp-servlet/web/target/*.war apache-tomcat-9.0.93/webapps/synclite-sample-app.war
-cp -f ../tools/synclite-dbreader/*.war apache-tomcat-9.0.93/webapps/synclite-dbreader.war
-cp -f ../tools/synclite-qreader/*.war apache-tomcat-9.0.93/webapps/synclite-qreader.war
-cp -f ../tools/synclite-jobmonitor/*.war apache-tomcat-9.0.93/webapps/synclite-jobmonitor.war
+cp -f ../lib/consolidator/synclite-consolidator-*.war apache-tomcat-9.0.95/webapps/synclite-consolidator.war
+cp -f ../sample-apps/synclite-logger/jsp-servlet/web/target/*.war apache-tomcat-9.0.95/webapps/synclite-sample-app.war
+cp -f ../tools/synclite-dbreader/*.war apache-tomcat-9.0.95/webapps/synclite-dbreader.war
+cp -f ../tools/synclite-qreader/*.war apache-tomcat-9.0.95/webapps/synclite-qreader.war
+cp -f ../tools/synclite-jobmonitor/*.war apache-tomcat-9.0.95/webapps/synclite-jobmonitor.war
 
